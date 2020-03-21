df5d1ead361d09085a86bf8f671ddb233a86286b
ad <rafin_ala03@hotmail.fr>
2020-03-21 02:17:19 +0100

    confirm registration

---
+++ src/Action/Security/ConfirmRegistration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created By IJ
+ * @author : Ala Daly <ala.daly@dotit-corp.com>
+ * @date : 21‏/3‏/2020, Sat
+ **/
+
+namespace App\Action\Security;
+
+
+class ConfirmRegistration
+{
+
+}
---
+++ src/Service/Mailer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created By IJ
+ * @author : Ala Daly <ala.daly@dotit-corp.com>
+ * @date : 21‏/3‏/2020, Sat
+ **/
+
+class Mailer
+{
+
+}
