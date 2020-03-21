864585dd5f9ff2675f6fc367eaa563fe48f7af08
ad <rafin_ala03@hotmail.fr>
2020-03-21 02:20:57 +0100

    confirm registartion

--- src/Service/MailerInterface.php
+++ src/Service/MailerInterface.php
@@ -1,11 +1,42 @@
 <?php
+
+namespace App\Service;
+
 /**
  * Created By IJ
  * @author : Ala Daly <ala.daly@dotit-corp.com>
  * @date : 21‏/3‏/2020, Sat
  **/
 
-class MailerInterface
+interface MailerInterface
 {
+    /**
+     * Send email
+     *
+     * @param string $subject
+     * @param $recipient
+     * @param $body
+     */
+    public function sendEmail(string $subject, $recipient, $body): void;
 
+    /**
+     * Send email
+     *
+     * @param string $subject
+     * @param $recipient
+     * @param $body
+     * @param $attachment
+     */
+    public function sendEmailWithAttachment(string $subject, $recipient, $body, $attachment):void ;
+
+    /**
+     * Get the mailer user email address
+     *
+     * @return string
+     */
+    public function getMailerUser(): string;
+
+    public function getFrontEndServerUrl():string ;
+
+    public function getBackEndServerUrl():string  ;
 }
