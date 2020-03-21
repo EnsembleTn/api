4872d19429c16f256ad41b89d6254e528fa622d7
gdalyy <daly.ghaith@gmail.com>
2020-03-21 16:33:17 +0100

    adding migration file for lastLogin filed in doctor entity

---
+++ src/Migrations/Version20200321141246.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200321141246 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE doctor ADD last_login DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE response CHANGE patient_id patient_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE doctor DROP last_login');
+        $this->addSql('ALTER TABLE response CHANGE patient_id patient_id INT NOT NULL');
+    }
+}
