471bb6af1f6118f87b45a1f6f61463925838ace2
moumniheithem <moumniheithem@gmail.com>
2020-03-24 00:28:02 +0100

    refacto

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,19 +12,19 @@ class AppFixtures extends Fixture
     {
         $questions = [
             [
-                "fr_value" => "Étiez vous à l’étranger récemment ? (oui, non)",
+                "fr_value" => "Étiez vous à l’étranger récemment ?",
                 "ar_value" => "هل كنت  في الخارج مؤخرًا ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_GENERAL
             ],
             [
-                "fr_value" => "Étiez vous en contact avec une personne venant de l’étranger ? (oui, non)",
+                "fr_value" => "Étiez vous en contact avec une personne venant de l’étranger ?",
                 "ar_value" => "هل كنت على اتصال بشخص قادم  من الخارج ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_GENERAL
             ],
             [
-                "fr_value" => "Étiez en contact avec une personne qui a été testée positive ? (oui, non)",
+                "fr_value" => "Étiez en contact avec une personne qui a été testée positive ?",
                 "ar_value" => "هل كنت على اتصال بشخص كانت نتيجة اختباره إيجابية ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_GENERAL
@@ -36,61 +36,61 @@ class AppFixtures extends Fixture
                 "category" => Question::CATEGORY_GENERAL
             ],
             [
-                "fr_value" => "Êtes vous diabétique ? (oui, non)",
+                "fr_value" => "Êtes vous diabétique ?",
                 "ar_value" => "هل تعاني من مرض السكري ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Avez vous une maladie respiratoire ? êtes vous suivi par un pneumologue ? (oui, non)",
+                "fr_value" => "Avez vous une maladie respiratoire ? êtes vous suivi par un pneumologue ?",
                 "ar_value" => "هل تعاني من مرض تنفسي؟ هل يتابعك طبيب مختص في أمراض الرئة ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ? (oui, non, je ne sais pas)",
+                "fr_value" => "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?",
                 "ar_value" => "هل تعاني من ارتفاع ضغط الدم؟ أو لديك مرض القلب أو الأوعية الدموية؟ هل  تأخذ علاج للقلب ؟",
                 "type" => Question::TYPE_YES_OR_NO_NEUTRAL,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Avez-vous une insuffisance rénale chronique dialysée ? (oui, non)",
+                "fr_value" => "Avez-vous une insuffisance rénale chronique dialysée ?",
                 "ar_value" => "هل تعاني من قصور كلوي ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Avez-vous une maladie chronique de foie ? (oui, non)",
+                "fr_value" => "Avez-vous une maladie chronique de foie ?",
                 "ar_value" => "هل لديك مرض مزمن في الكبد ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Avez vous ou avez vous eu un cancer ? (oui, non)",
+                "fr_value" => "Avez vous ou avez vous eu un cancer ?",
                 "ar_value" => "هل لديك أو كان  لديك مرض سرطان ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Êtes vous enceinte ?(oui, non, non applicable) ?",
+                "fr_value" => "Êtes vous enceinte ?",
                 "ar_value" => "هل أنت حامل ؟",
                 "type" => Question::TYPE_YES_OR_NO_NOT_APPLICABLE,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Avez-vous une maladie connue diminuer vos défenses immunitaires ? (oui, non)",
+                "fr_value" => "Avez-vous une maladie connue diminuer vos défenses immunitaires ?",
                 "ar_value" => "هل تعاني من مرض ينقص من مناعتك؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Prenez vous un traitement immunosuppresseur ? (oui, non)",
+                "fr_value" => "Prenez vous un traitement immunosuppresseur ?",
                 "ar_value" => "هل تتناول علاج مثبط للمناعة ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_ANTECEDENT
             ],
             [
-                "fr_value" => "Avez-vous de la fièvre, des frissons, des sueurs ? (oui, non)",
+                "fr_value" => "Avez-vous de la fièvre, des frissons, des sueurs ?",
                 "ar_value" => "هل تعاني من الحمى والرعشة والتعرق ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_SYMPTOMS
@@ -102,37 +102,37 @@ class AppFixtures extends Fixture
                 "category" => Question::CATEGORY_SYMPTOMS
             ],
             [
-                "fr_value" => "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ? (oui, non)",
+                "fr_value" => "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?",
                 "ar_value" => "هل تعاني من السعال  أو زيادة في السعال المعتاد  في الأيام القليلة الماضية ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_SYMPTOMS
             ],
             [
-                "fr_value" => "Avez-vous un mal de gorge apparu ces derniers jours ? (oui, non)",
+                "fr_value" => "Avez-vous un mal de gorge apparu ces derniers jours ?",
                 "ar_value" => "هل أصبت بالتهاب حنجرة  في الأيام الأخيرة ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_SYMPTOMS
             ],
             [
-                "fr_value" => "Avez-vous des maux de tête ? (oui, non)",
+                "fr_value" => "Avez-vous des maux de tête ?",
                 "ar_value" => "هل تعاني من آلام بالرأس ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_SYMPTOMS
             ],
             [
-                "fr_value" => "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ? (oui, non)",
+                "fr_value" => "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?",
                 "ar_value" => "هل عانيت من آلام أو آلام عضلية غير معتادة في الأيام القليلة الماضية ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_SYMPTOMS
             ],
             [
-                "fr_value" => "Avez-vous une fatigue inhabituelle ces derniers jours ? (oui, non)",
+                "fr_value" => "Avez-vous une fatigue inhabituelle ces derniers jours ?",
                 "ar_value" => "هل تعاني من تعب غير معتاد  في الأيام الأخيرة ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_SYMPTOMS
             ],
             [
-                "fr_value" => "Avez-vous une gêne respiratoire ou une augmentation de votre gêne respiratoire inhabituelle ? (oui, non)",
+                "fr_value" => "Avez-vous une gêne respiratoire ou une augmentation de votre gêne respiratoire inhabituelle ?",
                 "ar_value" => "هل  تعاني من صعوبة في التنفس ؟",
                 "type" => Question::TYPE_YES_OR_NO,
                 "category" => Question::CATEGORY_SYMPTOMS
